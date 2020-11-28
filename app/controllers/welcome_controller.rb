8de4c44a203e6a1d55f949eda438e0df87d5d3fc
Emily Chen <emilycwx@mit.edu>
2020-11-28 17:16:40 -0600

    adding all of the new pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def choiceA
+  end
+
+  def choiceB
+  end
+
+  def choiceC
+  end
+
+  def choiceD
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :pages
   resources :options
+  get '/choiceA' => 'welcome#choiceA'
+  get '/choiceB' => 'welcome#choiceB'
+  get '/choiceC' => 'welcome#choiceC'
+  get '/choiceD' => 'welcome#choiceD'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
